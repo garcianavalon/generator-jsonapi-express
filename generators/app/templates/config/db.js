6f36466cc6be852a2cd00b455b932bcbdec778f1
Enrique Garcia Navalon <garcianavalon@gmail.com>
2017-04-04 16:29:39 +0200

    sem-fix the return outside of funcion linter error

--- generators/app/templates/config/db.js
+++ generators/app/templates/config/db.js
@@ -1,8 +1,9 @@
 if (process.env.NODE_ENV === 'test') {
   console.warn('NODE_ENV environment variable is set to \'test\' so database connection will be skipped');
-  return;
+  // TODO(garcianavalon) once I manage to make slint ignore this error ...
+  // return;
+} else {
+  // Const debug = require('debug')('jsonapi:db');
+
+  // Your database's config and connection goes here.
 }
-
-const debug = require('debug')('jsonapi:db');
-
-// Your database's config and connection goes here.
